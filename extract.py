619b0c1ce2a05bc0f3aaae589a0f8acc0b1baf67
Raphaël Doursenaud <rdoursenaud@free.fr>
2015-05-15 04:45:36 +0200

    Added a python script to extract files from a show

---
+++ extract.py
@@ -0,0 +1,74 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+"""A CLI tool to extract Soundcraft Show files"""
+
+__author__ = 'Raphaël Doursenaud'
+
+import argparse
+import sys
+
+from bs4 import BeautifulSoup
+from base64 import b64decode
+from zlib import decompress
+from os.path import basename, splitext, join, exists
+from os import mkdir
+
+parser = argparse.ArgumentParser(description="Extract Soundcraft Show files.")
+parser.add_argument('infiles', metavar="INFILE", type=argparse.FileType('r'),
+                    default=sys.stdin, nargs='+',
+                    help="The show file(s) to extract.")
+
+args = parser.parse_args()
+
+for file in args.infiles:
+    print("Processing " + file.name + ", ", end="")
+
+    soup = BeautifulSoup(file, features='xml')
+    collection = soup('MHxDFile')
+    size = len(collection)
+    outfilepath = basename(file.name)
+    
+    # Remove extension
+    outfilepath = splitext(outfilepath)[0]
+
+    print("found " + str(size) + " files")
+
+    # Prepare output folder
+    if not exists(outfilepath):
+        mkdir(outfilepath)
+
+    for entry in collection:
+        filename = entry['name']
+
+        # Extract only filename from path
+        filename = filename.rsplit('\\', 1)[-1]
+
+        print("Processing (decoding & decompressing) " + filename + " in ./" + outfilepath + "/")
+
+        checksum = int(entry['checksum'])
+        datalength = int(entry['datalength'])
+        contents = entry.contents[0]
+
+        # Base64 decode
+        contents = b64decode(contents, validate=True)
+
+        # zlib decompress
+        contents = decompress(contents)
+
+        # TODO: Validate the data against the checksum when identified
+
+        if len(contents) != datalength:
+            raise Exception("Data size missmatch!")
+
+        # Write result to file
+        outfile = join(outfilepath, filename)
+        if exists(outfile):
+            answer = input("File " + outfile + " already exists! Overwrite? [y/N]")
+            if not (answer == 'Y' or answer == 'y'):
+                print("Skipping…")
+                continue
+        fh = open(outfile, 'wb')
+        fh.write(contents)
+        fh.close()
+
+
